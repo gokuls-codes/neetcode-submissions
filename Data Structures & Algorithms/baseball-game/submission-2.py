efdd63e5fc5efb84ecbfa2306cfb7b4a77ca410f
gk <88606340+gokuls-codes@users.noreply.github.com>
2026-05-23 14:45:21 +0530

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isInteger(self, s):
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
+
+    def calPoints(self, operations: List[str]) -> int:
+        mem = []
+        for op in operations:
+            if self.isInteger(op):
+                mem.append(int(op))
+            elif op == "+":
+                mem.append(mem[-1] + mem[-2])
+            elif op == "D":
+                mem.append(2 * mem[-1])
+            elif op == "C":
+                mem.pop()
+
+        return sum(mem)
